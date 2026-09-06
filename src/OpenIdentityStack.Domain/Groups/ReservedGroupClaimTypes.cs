c95b0005353c94352a9ed5160dbfce09d7823ca3
Tjeerd-menno Douma <tjeerd.menno@gmail.com>
2026-09-06 13:11:18 +0200

    Reserve nonce from group claim mappings

--- src/OpenIdentityStack.Domain/Groups/ReservedGroupClaimTypes.cs
+++ src/OpenIdentityStack.Domain/Groups/ReservedGroupClaimTypes.cs
@@ -6,7 +6,7 @@ public static class ReservedGroupClaimTypes
     private static readonly HashSet<string> reserved = new(StringComparer.OrdinalIgnoreCase)
     {
         "sub", "iss", "aud", "azp", "client_id", "scope", "scp", "permission", "permissions",
-        "role", "roles", "auth_time", "amr", "acr", "sid", "session_id", "exp", "iat", "nbf", "jti",
+        "role", "roles", "auth_time", "amr", "acr", "nonce", "sid", "session_id", "exp", "iat", "nbf", "jti",
         "email", "email_verified", "phone_number", "phone_number_verified", "ois_human_authenticated_at", "auth_method", "provider",
         "name", "given_name", "family_name", "middle_name", "nickname", "preferred_username", "profile", "picture",
         "website", "gender", "birthdate", "zoneinfo", "locale", "address", "updated_at",
